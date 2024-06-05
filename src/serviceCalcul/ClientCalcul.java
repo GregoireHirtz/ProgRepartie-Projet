9aa69e6479052e7142a17a0ec2aee68a546596b6
Fiera <46507472+Fiera42@users.noreply.github.com>
2024-06-05 11:08:06 +0200

    squel client calcul

---
+++ src/serviceCalcul/ClientCalcul.java
@@ -0,0 +1,12 @@
+package serviceCalcul;
+
+import interfaces.ServiceCalcul;
+import raytracer.Image;
+import raytracer.Scene;
+
+public class ClientCalcul implements ServiceCalcul {
+    @Override
+    public Image effectuerCalcul(Scene scene, int x, int y, int x2, int y2) {
+        return null;
+    }
+}
