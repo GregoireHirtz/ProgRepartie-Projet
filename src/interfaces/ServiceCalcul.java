9e28472751ea9074674fdd399b89a043d5736092
GregoireHirtz <hirtz.gregoire@ghmail.fr>
2024-06-05 11:05:15 +0200

    maj interface

--- src/interfaces/ServiceCalcul.java
+++ src/interfaces/ServiceCalcul.java
@@ -4,5 +4,5 @@ import raytracer.Image;
 import raytracer.Scene;
 
 public interface ServiceCalcul {
-    Image effectuerCalcul(Scene scene, int x, int y);
+    Image effectuerCalcul(Scene scene, int x, int y, int x2, int y2);
 }
--- src/interfaces/ServiceRayTracing.java
+++ src/interfaces/ServiceRayTracing.java
@@ -4,6 +4,6 @@ import raytracer.Image;
 import raytracer.Scene;
 
 public interface ServiceRayTracing {
-    Image calculerScene(Scene scene);
+    Image calculerScene(Scene scene, int hauteur, int largeur);
     void enregistrerServiceCalcul(ServiceCalcul service);
 }
